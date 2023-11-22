aa8dbf7d5251c56b08cf52b3f6f56aa5c78719ae
kwibisam <kwibisam04@gmail.com>
2023-11-23 01:18:05 +0200

    xml-validation-fix

--- app/api/voice/route.js
+++ app/api/voice/route.js
@@ -2,7 +2,10 @@ import { NextResponse } from 'next/server'
 export const GET = async(request, response) => {
     try {
 
-        return new NextResponse(`<Resource> <Say> hello there </Say> </Resource>`, {
+        return new NextResponse(`<?xml version="1.0" encoding="UTF-8"?>
+        <Response>
+                <Say>Hello, Hi there world!</Say>
+        </Response>`, {
             headers: {
                 "Content-Type": "text/xml"
             }
